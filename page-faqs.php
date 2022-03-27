94dea7c179afeb5e8dc283f2cb58650cb0a6d461
ivankam123 <94932102+ivankam123@users.noreply.github.com>
2022-03-26 17:59:20 -0700

    completed FAQs

--- page-faqs.php
+++ page-faqs.php
@@ -20,22 +20,41 @@ get_header();
 		<?php
 		while ( have_posts() ) : the_post(); ?>
 
+			<section class="faq-main">
 			<!-- <h1>Frequently Asked Questions</h1> -->
 			<h1><?php the_title()?></h1>
 
 			<?php
 			if (function_exists('get_field')) {
+				echo '<section class="faq-content">';
+				echo '<div class="faq-hero-img">';
+					the_post_thumbnail();
+				echo '</div>';
+				echo '<div class="faq-questions">';
 				if( have_rows('faqs') ): 
 					while ( have_rows('faqs') ) : the_row();
 						$sub_question = get_sub_field('question');
 						$sub_answer = get_sub_field('answer');
 						echo'<article>';
-							echo '<p>' .$sub_question.'</p>';
-							echo '<p>' .$sub_answer.'</p>';
+							echo '<p class="faq-question">' .$sub_question.'</p>';
+							echo '<p class="faq-answer">' .$sub_answer.'</p>';
 						echo '</article>';
 					endwhile;
 				endif;
+				?>
+				<div class="cta-check-out-tours">
+					<h2>Ready to Book?</h2>
+					<a href="<?php echo esc_url(get_page_link(50));?>" class="primary">Our Tours</a>
+					
+				</div>
+
+				<?php 
+				echo '</div>';
+				echo '</section>';
 			}
+			?>
+			</section>
+			<?php
 
 		endwhile; // End of the loop.
 		?>
